36efac67d72272e8e73c55baa59998d048f9f73a
Christian <chnicoloso@gmail.com>
2017-08-18 22:35:26 -0700

    removed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import MainView from './app/MainView';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import appReducer from './reducers/appReducer';
 
 ReactDOM.render( <MainView />, document.getElementById('root'));
