fe3ca824d5127aa31013e66feb2e6b7311df8819
Christian <chnicoloso@gmail.com>
2017-08-18 22:37:33 -0700

    removed reducers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainView from './app/MainView';
 // import registerServiceWorker from './registerServiceWorker';
-import appReducer from './reducers/appReducer';
+// import appReducer from './reducers/appReducer';
 
 ReactDOM.render( <MainView />, document.getElementById('root'));
 registerServiceWorker();
